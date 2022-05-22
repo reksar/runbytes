674c8a34a5cdb64360e8439e5073dd06842ed3db
reksarka <reksarka@gmail.com>
2022-05-22 20:35:24 +0300

    Refactoring: Loader.

--- runbytes/loader.py
+++ runbytes/loader.py
@@ -1,27 +1,27 @@
-import io
-import base64
-import zipfile
-import importlib.abc
-import importlib.util
-import importlib.abc
 import importlib.util
 
+from importlib.abc import InspectLoader
 from . import path
 
 
-class Loader(importlib.abc.InspectLoader):
+class Loader(InspectLoader):
 
     def __init__(self, app):
         self.app = app
 
     def get_source(self, fullname):
-        is_package = self.is_package(fullname)
-        app_path = path.from_package_name(fullname) + '__init__.py' if is_package else path.from_module_name(fullname)
-        try:
-            source_bytes = self.app.read(app_path)
-        except KeyError:
-            raise ImportError(f"no module {fullname}", name=fullname)
-        return importlib.util.decode_source(source_bytes)
+        return importlib.util.decode_source(self.source_bytes(fullname))
 
     def is_package(self, fullname):
         return path.contains(f"{fullname}.__init__", self.app)
+
+    def path(self, fullname):
+        if self.is_package(fullname):
+            return path.from_package_name(fullname) + '__init__.py'
+        return path.from_module_name(fullname)
+
+    def source_bytes(self, fullname):
+        try:
+            return self.app.read(self.path(fullname))
+        except KeyError:
+            raise ImportError(f"No module: {fullname}", name=fullname)
